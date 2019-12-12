dfde8384356ef379a27aa5bbe93472dd056590bb
Kuss <2360314753@qq.com>
2019-12-12 23:14:08 +0800

    :sparkles: feat: add @/components/HeaderBar

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -1,29 +1,75 @@
-import React from 'react';
+import React from 'react'
 import {
   AppBar,
   Toolbar,
   Typography,
   IconButton,
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+  useScrollTrigger,
+  Slide
+} from '@material-ui/core'
+import {
+  Menu as MenuIcon,
+  Add as AddIcon,
+  Close as CloseIcon
+} from '@material-ui/icons'
+
+const appName = 'Tangerine'
+
+interface WindowProps {
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+const HideOnScroll: React.FC<WindowProps> = (props) => {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 
 interface IProps {
   title?: string;
+  onMenu?: () => void;
+  onAdd?: () => void;
+  addTaskOpen?: boolean;
 }
 
-const appName = 'Tangerine';
+const HeaderBar: React.FC<IProps> = ({ title, onMenu, onAdd, addTaskOpen }) => (
+  <HideOnScroll>
+    <AppBar position="fixed">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={onMenu}
+        >
+          <MenuIcon />
+        </IconButton>
+        
+        <Typography style={{ flexGrow: 1 }}>{title ? title : appName}</Typography>
 
-const HeaderBar: React.FC<IProps> = ({ title }) => (
-  <AppBar>
-    <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography>
-        {title ? title : appName}
-      </Typography>
-    </Toolbar>
-  </AppBar>
+        {onAdd && 
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={onAdd}
+          >
+            {addTaskOpen === true ? 
+              <CloseIcon />
+            :
+              <AddIcon />
+            }
+          </IconButton>
+        }
+      </Toolbar>
+    </AppBar>
+  </HideOnScroll>
 )
 
-export default HeaderBar;
+export default HeaderBar
