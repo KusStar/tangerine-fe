cce4af26870ddac7202ee337589ab96cb5b36fca
Kuss <2360314753@qq.com>
2019-12-13 21:25:08 +0800

    :sparkles: feat: add @/components/Snackbar

---
+++ src/components/Snackbar/index.tsx
@@ -0,0 +1,28 @@
+import React  from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+
+export interface IProps {
+  message?: string;
+  onClose?: () => void;
+  open: boolean;
+  handleClose?: () => void;
+}
+
+const CustromSnackbar: React.FC<IProps> = (props) => {
+  const { message, onClose, open, handleClose, ...other } = props;
+  
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      message={message}
+    />
+  );
+}
+
+export default CustromSnackbar;
