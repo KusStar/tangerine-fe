2fd701fe10cd880c7f1be022332550fa1e87cde4
Kuss <2360314753@qq.com>
2019-12-16 11:44:59 +0800

    :sparkles: feat: @components/useLongPress -> handle onLongPress

---
+++ src/components/useLongPress/index.ts
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, MouseEvent } from 'react'
+
+export type CurrentTarget = HTMLElement | null
+
+const useLongPress = (callback = (t: CurrentTarget) => {}, ms = 300) => {
+  const [startLongPress, setStartLongPress] = useState<boolean>(false);
+  const [currentTarget, setCurrentTarget] = useState<CurrentTarget>(null);
+
+  useEffect(() => {
+    let timer: any;
+    if (startLongPress) {
+      timer = setTimeout(() => callback(currentTarget), ms);
+    } else {
+      setCurrentTarget(null);
+      clearTimeout(timer);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [startLongPress]);
+
+
+  const handleStartEvent = (event: MouseEvent<HTMLElement>) => {
+    setStartLongPress(true);
+    setCurrentTarget(event.currentTarget);
+  }
+  return {
+    onMouseDown: handleStartEvent,
+    onMouseUp: () => setStartLongPress(false),
+    onMouseLeave: () => setStartLongPress(false),
+    onTouchStart: handleStartEvent,
+    onTouchEnd: () => setStartLongPress(false),
+  };
+}
+
+
+export default useLongPress;
