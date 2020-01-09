8a1c372ea415293a0e7ff5bd1f31a3590b03ae2e
Kuss <2360314753@qq.com>
2020-01-09 18:32:11 +0800

    :zap: optimize: split chunks

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, '../src/index.tsx')
+    main: path.resolve(__dirname, '../src/index.tsx'),
+    useLongPress: path.resolve(__dirname, '../src/components/useLongPress'),
+    HeaderBar: path.resolve(__dirname, '../src/components/HeaderBar'),
+    TaskItem: path.resolve(__dirname, '../src/components/TaskItem')
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
@@ -28,9 +31,9 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
+          }
+        ]
+      },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html')
@@ -42,4 +45,26 @@ module.exports = {
     },
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   }
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
 }
