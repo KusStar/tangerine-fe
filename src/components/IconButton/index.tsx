475d9cc88d9352ed5bc6ff0e853a075b67cc3288
Kuss <2360314753@qq.com>
2020-01-09 19:51:26 +0800

    :lipstick: update: add @/components/IconButton

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode, MouseEventHandler, CSSProperties } from 'react'
+import IconButton from '@material-ui/core/IconButton'
+
+export interface IProps {
+  onClick?: MouseEventHandler
+  children: ReactNode
+  style?: CSSProperties
+}
+
+const CustomIconButton: React.FC<IProps> = props => {
+  const { onClick, children, style } = props
+
+  return (
+    <IconButton
+      edge="start"
+      color="inherit"
+      onClick={onClick}
+      style={style}
+    >
+      {children}
+    </IconButton>
+  )
+}
+
+export default CustomIconButton
